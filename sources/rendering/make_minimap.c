1ea677e9482ce3c6b04de0c4487400ea74b8d8a3
Lilly Kunne <sikunne@c1r4s4.42wolfsburg.de>
2025-07-22 17:53:15 +0200

    [ADD] #45 - in the middle of soemthing

--- sources/rendering/make_minimap.c
+++ sources/rendering/make_minimap.c
@@ -6,46 +6,13 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 12:42:58 by sikunne           #+#    #+#             */
-/*   Updated: 2025/07/22 03:17:21 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/07/22 17:07:58 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/cub3d.h"
 # define MMAP_SIZE 150
 # define MMAP_SCALE 10
-
-// draw a box at <x> <y> with <color>
-// Coordinates should be upscaled with BOX_SIZE
-// (2,1 = 2*BOX_SIZE, 1*BOX_SIZE)
-/*
-static void	st_draw_box(t_data *data, int color, int x, int y)
-{
-	
-}
-
-static void	st_draw_direction(t_data *data)
-{
-	double	x;
-	double	y;
-	int		i;
-	int		j;
-
-	x = (MOVE_SPEED * 5 * cos(deg_to_rad(data->rot)));
-	y = (MOVE_SPEED * 5 * sin(deg_to_rad(data->rot)));
-	x = (data->pos_x + x) * BOX_SIZE;
-	y = (data->pos_y + y) * BOX_SIZE;
-	if (y < 0 || y + PLAYER_SIZE > WINDOW_HEIGHT || \
-x < 0 || x + PLAYER_SIZE > WINDOW_WIDTH)
-		return ;
-	i = -1;
-	while (++i < PLAYER_SIZE)
-	{
-		j = -1;
-		while (++j < PLAYER_SIZE)
-			my_pixel_put(data->image, x + j, y + i, to_rgb(184, 201, 50));
-	}
-}
-*/
 
 static void st_hor_line(t_data *data, int x, int y, int color)
 {
@@ -98,14 +65,14 @@ static void	st_draw_fov(t_data *data)
 	while (steps < 40)
 	{
 		if (x + steps * (data->dir_x + data->plane_x) < 0 || \
-x + steps * (data->dir_x + data->plane_x) > (double)MMAP_SIZE || \
+x + steps * (data->dir_x + data->plane_x) > (double)(MMAP_SIZE - 1) || \
 y + steps * (data->dir_y + data->plane_y) < 0 || \
-y + steps * (data->dir_y + data->plane_y) > (double)MMAP_SIZE)
+y + steps * (data->dir_y + data->plane_y) > (double)(MMAP_SIZE - 1))
 			break ;
 		if (x + steps * (data->dir_x - data->plane_x) < 0 || \
-x + steps * (data->dir_x - data->plane_x) > (double)MMAP_SIZE || \
+x + steps * (data->dir_x - data->plane_x) > (double)(MMAP_SIZE - 1) || \
 y + steps * (data->dir_y - data->plane_y) < 0 || \
-y + steps * (data->dir_y - data->plane_y) > (double)MMAP_SIZE)
+y + steps * (data->dir_y - data->plane_y) > (double)(MMAP_SIZE - 1))
 			break ;
 		my_pixel_put(data->image, (int)(x + steps * (data->dir_x + data->plane_x)), (int)(y + steps * (data->dir_y + data->plane_y)), to_rgb(0, 0, 0));
 		my_pixel_put(data->image, (int)(x + steps * (data->dir_x - data->plane_x)), (int)(y + steps * (data->dir_y - data->plane_y)), to_rgb(0, 0, 0));
@@ -130,22 +97,28 @@ static void	st_draw_player(t_data *data)
 	st_draw_fov(data);
 }
 
-void	make_minimap(t_data *data)
+static int	st_convert(int current, int max)
+
+static void st_draw_blocks(t_data *data)
 {
-	/*
-	int		x;
-	int		y;
+	int	x;
+	int y;
 
 	y = -1;
-	while (++y < MMAP_SIZE)
+	while (++y < MMAP_SCALE)
 	{
 		x = -1;
-		while (++x < MMAP_SIZE)
+		while (++x < MMAP_SCALE)
 		{
-			my_pixel_put(data->image, x, y, to_rgb(166, 130, 96));
+			turn 0-MMAP_SCALE
+			to -1 - +1;
 		}
 	}
-		*/
+}
+
+void	make_minimap(t_data *data)
+{
+	st_draw_blocks(data);
 	st_draw_player(data);
 	st_draw_frame(data);
 }
