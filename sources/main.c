89ec5acb841aba3d702e02eec4bc0f6887f41680
Lilly Kunne <sikunne@c1r1s2.42wolfsburg.de>
2025-07-28 18:20:48 +0200

    [ADD] #72 - Changed Exit Code when encountering parsing error

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 15:27:06 by sikunne           #+#    #+#             */
-/*   Updated: 2025/07/01 14:14:33 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/07/28 18:20:18 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ int	main(int argc, char **argv)
 	t_map		map;
 
 	if (!argc_check(argc))
-		return (0);
+		return (1);
 	data.minfo = &map;
 	status = load_map(argv[1], &map);
 	mapfile_complain(status);
 	if (status != 0)
-		return (0);
+		return (1);
 	data_setup(&data);
 	mlx_loop(data.mlx);
 	return (0);
